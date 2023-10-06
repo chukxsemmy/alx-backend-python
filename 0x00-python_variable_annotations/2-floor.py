3ed36b334847f658e364cf291922cab0f2a9017d
chukxsemmy <chukxsemmy@gmail.com>
2023-10-05 17:38:33 -0700

    returns the floor of a string

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""function that returns a concatenated string """
+import math
+
+
+def floor(n: float) -> int:
+    """returns the floor of a string"""
+    return math.floor(n)
